c6885eef914d3cdac4106b89934f9101b491f0e2
dafogaca <diibex@yahoo.com.br>
2021-08-12 20:39:29 -0300

    Criei teste para incluir um pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -10,6 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classes
 public class Pet {
@@ -38,6 +40,10 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Aquiles"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
         ;
     }
 }
