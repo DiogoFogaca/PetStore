6cf5604b65066a9566c5e13e8b2adb8efeca077c
dafogaca <diibex@yahoo.com.br>
2021-08-12 21:47:20 -0300

    Criei teste para incluir, alterar e consultar um pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -24,7 +24,7 @@ public class Pet {
     }
 
     // Incluir - Create - Post
-    @Test   // Identifica o método ou função como um teste para o TestNG
+    @Test(priority = 1)   // Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
@@ -42,8 +42,53 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Aquiles"))
                 .body("status", is("available"))
-                .body("category.name", is("dog"))
-                .body("tags.name", contains("sta"))
+                .body("category.name", is("AXXXRE330"))
+                .body("tags.name", contains("data"))
         ;
     }
+    @Test(priority = 3)
+    public void consultarPet(){
+        String petId = "1983040723";
+        String token =
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+        given()
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Aquiles"))
+                .body("status", is("available"))
+                .body("category.name", is("AXXXRE330"))
+                .body("tags.name", contains("data"))
+        .extract()
+                .path("category.name")
+        ;
+        System.out.println("O token gerado é:" + token);
+    }
+
+    @Test(priority = 2)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Aquiles"))
+                .body("status", is("sold"))
+        ;
+    }
+
+
+
 }
